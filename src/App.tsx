ae9e5d6e544d890c9186e0c8bb875c00940833ca
Pol Torres Alfonso <Pol.Torres.Alfonso@kyndryl.com>
2022-12-02 17:11:56 +0100

    fix: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './components/core/layout/layout';
-import { HomePage } from './components/pages/home/home';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/core/layout/layout";
+import { HomePage } from "./components/pages/home/home";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route path='/' element={<HomePage />}/>
-              <Route path='/homepage' element={<HomePage />}/>
-            </Route>
-          </Routes>
-        </BrowserRouter>
+    <BrowserRouter basename="personal-website">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/homepage" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -12,11 +12,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={mainTheme}>
-      <HashRouter>
+    <HashRouter>
+      <ThemeProvider theme={mainTheme}>
         <App />
-      </HashRouter>
-    </ThemeProvider>
+      </ThemeProvider>
+    </HashRouter>
   </React.StrictMode>
 );
 
