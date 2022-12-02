d0d9b855270f20ba6065610721e66aa36d4336ae
Pol Torres Alfonso <Pol.Torres.Alfonso@kyndryl.com>
2022-12-02 17:27:23 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
-    <HashRouter basename="personal-website">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<HomePage />} />
