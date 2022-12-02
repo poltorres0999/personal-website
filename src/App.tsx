d277c65b186e9746000468477f60a13860643a0a
Pol Torres Alfonso <Pol.Torres.Alfonso@kyndryl.com>
2022-12-02 17:51:06 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router, Navigate } from "react-router-dom";
 import Layout from "./components/core/layout/layout";
 import { HomePage } from "./components/pages/home/home";
 import { HashRouter } from "react-router-dom";
@@ -8,9 +8,12 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
+        <Route element={<Layout />}>
+          {" "}
+          {/* put url base here and nest children routes */}
+          <Route path="/personal-website" element={<HomePage />} />
         </Route>
+        {/* navigate to default route if no url matched */}
       </Routes>
     </HashRouter>
   );
