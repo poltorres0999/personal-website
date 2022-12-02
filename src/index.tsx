3f136e31036b910770ad2255282b37dff119dfdd
Pol Torres Alfonso <Pol.Torres.Alfonso@kyndryl.com>
2022-12-02 17:07:17 +0100

    fix: update rourting with hashroute

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import mainTheme from './style/theme';
-import { ThemeProvider } from '@mui/material';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import mainTheme from "./style/theme";
+import { ThemeProvider } from "@mui/material";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={mainTheme}>
-    <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </ThemeProvider>
-    
   </React.StrictMode>
 );
 
